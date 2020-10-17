6707acfe77aa7e95d74034d84928d64d3bd9d2a6
DedInsade <skok2783@gmail.com>
2020-10-18 04:53:50 +0700

    Third program

---
+++ Shakuchi.HelloWord/Shakuchi.HelloWord/Program_3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shakuchi.HelloWord
+{
+    class Program_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type something");
+            var result = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("You was wrote: {0}", result);
+            Console.ReadKey();
+            Console.Clear();
+        }        
+    }
+}
